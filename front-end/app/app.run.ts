2373c7559b425894fc3d4261f86d12c9c6a06465
Galyna <galynavistovska@gmail.com>
2016-07-13 15:49:14 +0300

    social shares

--- front-end/app/app.run.ts
+++ front-end/app/app.run.ts
@@ -2,6 +2,8 @@ import {IRootScope} from "../typings";
 appRun.$inject = ['$rootScope'];
 export function appRun($rootScope:IRootScope) {
     $rootScope.socialParams = {
+        host:"",
+        target:"",
         title: "",
         image: "",
         description: ""
--- front-end/app/ui/mediaObserver.service.ts
+++ front-end/app/ui/mediaObserver.service.ts
@@ -2,46 +2,51 @@
 import {IRootScope} from "../../typings";
 
 export interface IMediaObserverFactory {
-    observe(items: string[], index: number): void;
+    observe(items:string[], index:number, socialParams:any):void;
     close():void;
 }
 interface IScope extends ng.IScope {
-    vm: any,
-    items: string[];
+    vm:any,
+    items:string[];
 }
 
 export let MediaObserverFactoryName = 'mediaObserver';
 MediaObserverFactory.$inject = ['$compile', '$rootScope', '$templateCache'];
-export function MediaObserverFactory($compile: ng.ICompileService, $rootScope: IRootScope,
-                                     $templateCache: ng.ITemplateCacheService) {
-    var $scope = <IScope>$rootScope.$new(true);
+export function MediaObserverFactory($compile:ng.ICompileService, $rootScope:IRootScope,
+                                     $templateCache:ng.ITemplateCacheService) {
+    var $scope = <IScope>$rootScope.$new( true );
     $scope.vm = {
         prev: prev,
         next: next,
         close: close,
         isVideo: isVideo,
-        img: {}
+        img: {},
+        socialParams: {}
     };
-    var test = $templateCache.get('app/ui/mediaObserver.html');
-    var templateEl = angular.element(test);
-    var element = $compile(templateEl)($scope);
-    var body = angular.element(document.body);
-    body.prepend(element);
+    var test = $templateCache.get( 'app/ui/mediaObserver.html' );
+    var templateEl = angular.element( test );
+    var element = $compile( templateEl )( $scope );
+    var body = angular.element( document.body );
+    body.prepend( element );
 
     function prev() {
         $scope.vm.index = $scope.vm.index <= 0 ? $scope.vm.items.length - 1 : $scope.vm.index - 1;
         $scope.vm.img.current = $scope.vm.items[$scope.vm.index];
+        $scope.vm.socialParams.image = $scope.vm.socialParams.host + $scope.vm.img.current.url;
+        $scope.vm.socialParams.description = $scope.vm.img.current.name;
     }
 
     function next() {
         $scope.vm.index = $scope.vm.index >= $scope.vm.items.length - 1 ? 0 : $scope.vm.index + 1;
         $scope.vm.img.current = $scope.vm.items[$scope.vm.index];
+        $scope.vm.socialParams.image = $scope.vm.socialParams.host + $scope.vm.img.current.url;
+        $scope.vm.socialParams.description = $scope.vm.img.current.name;
     }
 
     function close() {
         $scope.items = null;
-        element.removeClass('visible');
-        body.removeClass('media-observer active');
+        element.removeClass( 'visible' );
+        body.removeClass( 'media-observer active' );
     }
 
     //TODO: implement
@@ -51,12 +56,15 @@ export function MediaObserverFactory($compile: ng.ICompileService, $rootScope: I
     }
 
     return <IMediaObserverFactory>{
-        observe: (items, index = 0) => {
+        observe: (items, index = 0, socialParams) => {
             $scope.vm.index = index;
             $scope.vm.items = items;
             $scope.vm.img.current = $scope.vm.items[$scope.vm.index];
-            element.addClass('visible');
-            body.addClass('media-observer active');
+            $scope.vm.socialParams = socialParams;
+            $scope.vm.socialParams.image = $scope.vm.socialParams.host + $scope.vm.img.current.url;
+            $scope.vm.socialParams.description = $scope.vm.img.current.name;
+            element.addClass( 'visible' );
+            body.addClass( 'media-observer active' );
         },
         close: close
     }
