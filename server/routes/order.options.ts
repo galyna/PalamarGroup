18eda032d146008d2104c0f8c7e7c10776f78bb0
Maozedong <maozedong@mail.com>
2016-09-18 14:15:30 +0300

    sms sending only in prod mode

--- server/routes/order.options.ts
+++ server/routes/order.options.ts
@@ -3,7 +3,8 @@ import {sms} from "../services/sms.service";
 import {IOrderModel} from "../models/order";
 
 export let orderOptions: IOptions = {
-    postCreate: (req:any, res, next) => {
+    postCreate(req:any, res, next) {
+        if (process.env.TYPE !== 'prod') return console.trace('Not prod env, skipping sms sending');
         const result = <IOrderModel>req.erm.result;
         //we're not waiting for result here, user don't need it
         sms.sendAdminNotification(`${result.event_name}. ${result.phone}`).catch(() => {});
