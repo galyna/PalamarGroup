35ee3d357f2fe4de31316da4cc5fb78ab172ab95
Maozedong <maozedong@mail.com>
2016-09-18 13:38:00 +0300

    dist folder removed from repo server config removed from repo .gitignore rules added

--- server/config.ts
+++ server/config.ts
@@ -11,7 +11,7 @@ export let config = {
         gatewayOptions: {
             publicKey: "68718f1c032ac94bfea8f305bd1849a1",
             privateKey: "0ed4c532a55744c7c7a2a0e1efb08f10",
-            url: "http://atompark.com/api/sms/"   
+            url: "http://atompark.com/api/sms/"
         },
         adminPhone: "+380963073149",
         sender: "PalamarG"
---
+++ server/sample.config.ts
@@ -0,0 +1,19 @@
+// let path = require('path');
+//
+// export let config = {
+//     origin: '',
+//     mongoUrl: '',
+//     appSecret: '',
+//     uploadDir: path.join(__dirname, ''),
+//     uploadsUrl: '',
+//     defaultQueryLimit: ,
+//     ePochta: {
+//         gatewayOptions: {
+//             publicKey: "",
+//             privateKey: "",
+//             url: ""   
+//         },
+//         adminPhone: "",
+//         sender: ""
+//     }
+// };
