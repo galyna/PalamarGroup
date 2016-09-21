e71574d8f3ab54b2528697798d79b538b17fa840
Galyna <galynavistovska@gmail.com>
2016-09-21 03:23:05 +0300

    order on contact

--- front-end/app/admin/academy/components/orders.component.ts
+++ front-end/app/admin/academy/components/orders.component.ts
@@ -15,7 +15,7 @@ let template = `
     <md-subheader class="md-no-sticky">Записи на курс</md-subheader>
     <md-list-item class="md-2-line" ng-repeat="order in $ctrl.orders" ng-click="$ctrl.showEditOrderDialog($event, order)">
         <div class="md-list-item-text" layout="column">
-            <h3>{{::order.name||'Анонім'}} {{::order.phone||order.email}}</h3>
+            <h3>{{::order.name||'Анонім'}} {{::order.phone||order.email||''}}</h3>
             <p>{{::$ctrl.getOrderTitle(order)}}</p>
         </div>
         <div class="md-secondary md-margin">
@@ -50,7 +50,7 @@ let editOrderDialogTemplate = `<md-dialog aria-label="Order edit" ng-cloak>
     <form name="orderEditForm" ng-submit="$ctrl.save(orderEditForm)">
         <md-toolbar>
             <div class="md-toolbar-tools">
-                <h2>{{$ctrl.order.event_name}}</h2>
+                <h2 ng-if="$ctrl.order.event_name">{{$ctrl.order.event_name}}</h2>
                 <span flex></span>
                 <md-button class="md-icon-button" ng-click="$ctrl.cancel()">
                     <md-icon md-svg-src="navigation:ic_close_24px" aria-label="Close dialog"></md-icon>
@@ -61,7 +61,7 @@ let editOrderDialogTemplate = `<md-dialog aria-label="Order edit" ng-cloak>
         <md-dialog-content>
             <div class="md-dialog-content" layout="row">
                 <md-subheader flex="1">
-                    <p>{{$ctrl.order.date|date}}</p>
+                    <p ng-if="$ctrl.order.date">{{$ctrl.order.date|date}}</p>
                     <p ng-if="$ctrl.order.name">
                         {{$ctrl.order.name}}
                     </p>
@@ -104,14 +104,14 @@ class EditOrderDialogController {
     private originalOrder:IOrder;
 
     constructor(private $mdDialog:ng.material.IDialogService, order:IOrder) {
-        this.order = angular.copy(order);
+        this.order = angular.copy( order );
         this.originalOrder = order;
     }
 
     save($form:ng.IFormController) {
         if ($form.$valid) {
-            angular.extend(this.originalOrder, this.order);
-            this.$mdDialog.hide(this.originalOrder);
+            angular.extend( this.originalOrder, this.order );
+            this.$mdDialog.hide( this.originalOrder );
         }
     }
 
@@ -129,7 +129,7 @@ export class AdminOrdersController {
 
     constructor(private $filter:ng.IFilterService, private $mdDialog:ng.material.IDialogService, private $mdToast:ng.material.IToastService,
                 private $mdMedia:ng.material.IMedia, private orderResource:IOrderResource,
-                private pagingService: PagingService) {
+                private pagingService:PagingService) {
 
     }
 
@@ -138,7 +138,7 @@ export class AdminOrdersController {
     }
 
     showEditOrderDialog(ev:MouseEvent, order:IOrder) {
-        this.$mdDialog.show({
+        this.$mdDialog.show( {
             template: editOrderDialogTemplate,
             controller: EditOrderDialogController,
             controllerAs: '$ctrl',
@@ -146,67 +146,73 @@ export class AdminOrdersController {
             locals: {
                 order: order
             },
-            parent: angular.element(document.body),
+            parent: angular.element( document.body ),
             targetEvent: ev,
-        }).then((order) => this.saveOrder(order));
+        } ).then( (order) => this.saveOrder( order ) );
     }
 
     saveOrder(order:IOrder) {
 
-        order.$save().then(() => {
-            this.$mdToast.showSimple(`Запис збережено`);
-        }).catch((err) => {
-            this.$mdToast.showSimple(err.message);
-        });
+        order.$save().then( () => {
+            this.$mdToast.showSimple( `Запис збережено` );
+        } ).catch( (err) => {
+            this.$mdToast.showSimple( err.message );
+        } );
     }
 
     showDeleteDialog(ev, order:IOrder) {
         let confirm = this.$mdDialog.confirm()
-            .title("Підтвердження дії")
-            .textContent(`Ви дійсно бажаєте видалити Запис ${order.name}?`)
-            .ariaLabel("Підтвердження дії")
-            .targetEvent(ev)
-            .ok('Так')
-            .cancel('Ні');
+            .title( "Підтвердження дії" )
+            .textContent( `Ви дійсно бажаєте видалити Запис ${order.name}?` )
+            .ariaLabel( "Підтвердження дії" )
+            .targetEvent( ev )
+            .ok( 'Так' )
+            .cancel( 'Ні' );
 
-        return this.$mdDialog.show(confirm)
-            .then(() => {
-                return this.deleteOrder(order);
-            });
+        return this.$mdDialog.show( confirm )
+            .then( () => {
+                return this.deleteOrder( order );
+            } );
     }
+
     deleteOrder(order:IOrder) {
 
-        order.$delete().then(() => {
-            this.$mdToast.showSimple(`Замовлення видалено`);
-        }).catch((err) => {
-            this.$mdToast.showSimple(err.message);
-        }).finally(()=> {
-            this.showPage(this.pagingService.currentPage());
-        });;
+        order.$delete().then( () => {
+            this.$mdToast.showSimple( `Замовлення видалено` );
+        } ).catch( (err) => {
+            this.$mdToast.showSimple( err.message );
+        } ).finally( ()=> {
+            this.showPage( this.pagingService.currentPage() );
+        } );
+        ;
     }
 
     getOrderTitle(order:IOrder) {
         let format = "dd.MM.yyyy";
-        let firstDate = this.$filter('date')(order.event_dates[0], format);
-        let lastDate = this.$filter('date')(order.event_dates[order.event_dates.length - 1], format);
-        return order.event_name + " " + firstDate + "-" + lastDate;
+        if (order.event_dates.length > 0) {
+            let firstDate = this.$filter( 'date' )( order.event_dates[0], format );
+            let lastDate = this.$filter( 'date' )( order.event_dates[order.event_dates.length - 1], format );
+            return order.event_name + " " + firstDate + "-" + lastDate;
+        } else {
+            return "";
+        }
     }
 
     prev() {
-        this.showPage(this.pagingService.prevPage());
+        this.showPage( this.pagingService.prevPage() );
     }
 
     next() {
-        this.showPage(this.pagingService.nextPage());
+        this.showPage( this.pagingService.nextPage() );
     }
 
     private showPage(page = 1) {
-        this.orders = this.orderResource.query({page: page, sort: {"answered":1, "date": -1}},
+        this.orders = this.orderResource.query( {page: page, sort: {"answered": 1, "date": -1}},
             (res, headers) => {
-                let {total, page, perPage} = this.pagingService.parseHeaders(headers);
-                this.pagingService.update({page: page, perPage: perPage, total: total});
-                this.paging = angular.copy(this.pagingService.params());
-            });
+                let {total, page, perPage} = this.pagingService.parseHeaders( headers );
+                this.pagingService.update( {page: page, perPage: perPage, total: total} );
+                this.paging = angular.copy( this.pagingService.params() );
+            } );
     }
 }
 
