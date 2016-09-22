1dc7033b1e5a35588237641ddd1544f03c723988
Galyna <galynavistovska@gmail.com>
2016-09-23 02:37:14 +0300

    images performance fixed

--- front-end/app/app.run.ts
+++ front-end/app/app.run.ts
@@ -1,8 +1,8 @@
 import {IRootScope} from "../typings";
 import AOS from "aos";
 
-appRun.$inject = ['$rootScope', '$timeout','$mdMedia'];
-export function appRun($rootScope:IRootScope, $timeout,$mdMedia) {
+appRun.$inject = ['$rootScope', '$timeout', '$mdMedia'];
+export function appRun($rootScope:IRootScope, $timeout, $mdMedia) {
     $rootScope.socialParams = {
         host: "",
         target: "",
@@ -11,17 +11,19 @@ export function appRun($rootScope:IRootScope, $timeout,$mdMedia) {
         description: ""
     };
 
-    $rootScope.isBigSize= $mdMedia( 'gt-md' );
-
+    $rootScope.isBigSize = $mdMedia( 'gt-md' );
+    if ($rootScope.isBigSize) {
+        var videoTag = angular.element( ` <video  hide show-gt-md="true" class="screen" muted autoplay
+                                              loop="true" onloadedmetadata="this.muted = true" >
+                                           <source type="video/mp4" src="../content/images/bg/bd.mp4"/>
+                                                           </video>` );
+        var videoContainer = angular.element( document.querySelector( '#video-container' ) );
+        videoContainer.append( videoTag );
+    }
     //create a new instance
     AOS.init( {
         disable: 'mobile'
     } );
-    //
-    // $window.onload = function(e) {
-    //
-    //     AOS.refresh();
-    // }
 
     $rootScope.$on( '$routeChangeSuccess', function () {
         $timeout( ()=> {
