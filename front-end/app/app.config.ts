f91a5ebda68bb8019a6c9aeb1db28acef0e0acce
Galyna <galynavistovska@gmail.com>
2016-10-25 20:23:55 +0300

    google map

--- front-end/app/app.config.ts
+++ front-end/app/app.config.ts
@@ -5,10 +5,6 @@ import {IRootScope} from "../typings";
 
 materialConfig.$inject = ['uiGmapGoogleMapApiProvider'];
 export function materialConfig(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure( {
-        //    key: 'your api key',
-        v: '3.20', //defaults to latest 3.X anyhow
-        libraries: 'weather,geometry,visualization'
-    } );
+  
 
 }
