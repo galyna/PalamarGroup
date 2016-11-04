1bc5d78e80e6865098084897a0b9603234cde1aa
Galyna <galynavistovska@gmail.com>
2016-11-04 20:15:13 +0200

    video fix

--- front-end/app/app.run.ts
+++ front-end/app/app.run.ts
@@ -21,14 +21,14 @@ export function appRun($rootScope:IRootScope, $timeout, $mdMedia) {
         var videoContainer = angular.element( document.querySelector( '#video-container' ) );
         videoContainer.append( videoTag );
     }
-    if ($mdMedia( 'gt-lg' )) {
-        var videoTag = angular.element( ` <video  hide show-gt-md="true" class="screen" muted autoplay
-                                              loop="true" onloadedmetadata="this.muted = true" >
-                                           <source type="video/mp4" src="../content/images/bg/bg.mp4"/>
-                                                           </video>` );
-        var videoContainer = angular.element( document.querySelector( '#video-container' ) );
-        videoContainer.append( videoTag );
-    }
+    // if ($mdMedia( 'gt-lg' )) {
+    //     var videoTag = angular.element( ` <video  hide show-gt-md="true" class="screen" muted autoplay
+    //                                           loop="true" onloadedmetadata="this.muted = true" >
+    //                                        <source type="video/mp4" src="../content/images/bg/bg.mp4"/>
+    //                                                        </video>` );
+    //     var videoContainer = angular.element( document.querySelector( '#video-container' ) );
+    //     videoContainer.append( videoTag );
+    // }
     //create a new instance
 
 
