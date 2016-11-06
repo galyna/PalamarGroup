798f6f002fbc9f820124bb60b0406ed5c4466c45
Galyna <galynavistovska@gmail.com>
2016-11-06 21:15:52 +0200

    pagging  fix

--- front-end/app/salon/components/salon.home.component.ts
+++ front-end/app/salon/components/salon.home.component.ts
@@ -69,7 +69,8 @@ const template = `<div class="courses-details description-container" layout="col
             <div class="courses-hear-forms" layout-margin layout layout-wrap layout-align="center center">
                 <md-card md-whiteframe="6"  ng-repeat="master in $ctrl.masters track by $index"
                          class="md-margin box "
-                         flex-gt-sm="46"
+                           ng-attr-flex-gt-sm="{{::$ctrl.getPictureFlex($index,$ctrl.master.works.length)}}"
+                       
                          flex-gt-xs="46" flex-xs="80"
                          ng-click="::$ctrl.showMaster(master._id)">
                     <div ng-if="master.rate && master.rate._id!=='0'"
@@ -120,7 +121,8 @@ const template = `<div class="courses-details description-container" layout="col
                 <div class="courses-hear-forms" layout-margin layout layout-wrap layout-align="center center">
                     <md-card md-whiteframe="6"  ng-repeat="photo in ::transform.photos track by $index"
                              class="md-margin "
-                             flex-gt-sm="46"
+                               ng-attr-flex-gt-sm="{{::$ctrl.getPictureFlex($index,$ctrl.master.works.length)}}"
+                       
                              flex-gt-xs="46" flex-xs="80"
                               ng-click="::$ctrl.showMediaObserver(transform.photos, $index)">
                         <img ng-src="{{::photo.url}}" class="md-card-image">
