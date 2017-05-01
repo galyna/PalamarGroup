b18391f9086ffc1d4abe829ab8368463a6f24683
galyna <galynavistovska@gmail.com>
2017-05-01 14:29:26 +0300

    s3 upload undone

--- server/services/photo.service.ts
+++ server/services/photo.service.ts
@@ -2,16 +2,17 @@ import {config} from '../config';
 let uuid = require('node-uuid');
 let fs = require('fs');
 let path = require('path');
+let AWS = require('aws-sdk');
 
-class PhotoService{
-    
+class PhotoService {
+
     private uploadDir: string;
     private static extFromPathRegExp = /.+\.(\w+)$/;
 
-    constructor (config, private uuid, private fs, private pathUtil){
+    constructor(config, private uuid, private fs, private pathUtil) {
         this.uploadDir = config.uploadDir;
     }
-    
+
     create(path: string) {
         let ext = path.match(PhotoService.extFromPathRegExp)[1];
         let newName = this.uuid.v4() + '.' + ext;
@@ -23,13 +24,42 @@ class PhotoService{
             });
         });
     }
-    
+
+    s3Upload(path: string) {
+        let ext = path.match(PhotoService.extFromPathRegExp)[1];
+        let newName = this.uuid.v4() + '.' + ext;
+        let newPath = this.uploadDir + '/' + newName;
+        return new Promise((resolve, reject) => {
+
+          // Read in the file, convert it to base64, store to S3
+            fs.readFile(path, function (err, data) {
+                if (err) {
+                    throw err;
+                }
+
+                var base64data = new Buffer(data, 'binary');
+
+                var s3 = new AWS.S3();
+                s3.client.putObject({
+                    Bucket: 'banners-adxs',
+                    Key: 'del2.txt',
+                    Body: base64data,
+                    ACL: 'public-read'
+                }, function (resp) {
+                    console.log(arguments);
+                    console.log('Successfully uploaded package.');
+                });
+
+            });
+        });
+    }
+
     remove(name: string) {
         let fileToDelete = this.uploadDir + '/' + name;
         console.log('removing file: ' + fileToDelete);
         return new Promise<void>((resolve, reject) => {
-            fs.unlink(fileToDelete, function(err){
-                if(err) {
+            fs.unlink(fileToDelete, function (err) {
+                if (err) {
                     return reject(err);
                 }
                 return resolve();
@@ -37,12 +67,12 @@ class PhotoService{
         });
     }
 
-    removeByUrl(url:string) {
+    removeByUrl(url: string) {
         let name = this.pathUtil.basename(url);
         return this.remove(name);
     }
 
-    path(name: string){
+    path(name: string) {
         return this.uploadDir + '/' + name;
     }
 
