b3af3aa7b31204d71abfb3aba2e1baf59d92a8bd
ostapn <ostapn@eze.tech>
2017-10-01 13:31:00 +0300

    master rate duplication

--- front-end/app/salon/components/favor.component.ts
+++ front-end/app/salon/components/favor.component.ts
@@ -128,6 +128,7 @@ const template = `
                                 <div hide show-sm="true" flex="90" class="md-headline capitalize">{{::master.name}}
                                 </div>
                             </div>
+                            <div hide show-gt-sm="true" class="md-title" style="margin-top: -45px" ng-bind="::master.rate.text"></div>
                             <div hide show-gt-sm="true" class="md-title">
                                 Вибери послугу та запишись
                             </div>
